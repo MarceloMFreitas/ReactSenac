110e0da08e7c4c24eb191e4ac772b9cd928f9bc6
MarceloMfreitas <prof.marcelomfreitas@gmail.com>
2025-11-11 16:36:02 -0300

    modified:   Projeto/petshop/src/App.jsx 	new file:   Projeto/petshop/src/assets/css/base/_reset.css 	new file:   Projeto/petshop/src/assets/css/base/base.css 	modified:   Projeto/petshop/src/main.jsx 	new file:   Projeto/petshop/src/pages/Home.jsx 	new file:   Projeto/petshop/src/pages/Sobre.jsx

---
+++ Projeto/petshop/src/pages/Sobre.jsx
@@ -0,0 +1,18 @@
+// não precisa fazer o import do React
+
+const Sobre = () => {
+    return(
+        <main>
+            <div>
+                <h2>Sobre Meu amigo cão</h2>
+            </div>
+        <section>
+            <article>
+                Na Meu amigo cão, o nosso sucesso como uma organização é conduzida pelo 
+                cuidado que temos com nossos animais.!!!
+            </article>
+        </section>
+        </main>
+    )
+}
+export default Sobre
